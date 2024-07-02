273715665a44470cdd86e311674ff4ea9824a73a
DiegoQuiF <diego.quinonez@unmsm.edu.pe>
2024-07-01 19:26:45 -0500

    Algunas funcionalidades de especialista implementadas

--- DSM_v1/src/routes/index.py
+++ DSM_v1/src/routes/index.py
@@ -3,11 +3,14 @@ from src.services.POST.postRegister import postRegister
 from src.services.POST.postLogin import postLogin
 from src.services.POST.postObtenerTest import postObtenerTest
 from src.services.POST.postRegTest import postRegTest
-from src.services.POST.postObtenerHistorias import postObtenerHistorias
-from src.services.POST.postObtenerTestHistoria import postObtenerTestHistoria
 from src.services.POST.postCompletarRegister import postCompletarRegister
+from src.services.POST.postObtenerPersona import postObtenerPersona
+from src.services.POST.postObtenerProvincias import postObtenerProvincias
+from src.services.POST.postObtenerUbigeos import postObtenerUbigeos
+from src.services.POST.postRegisterDiagEsp import postRegisterDiagEsp
+from src.services.GET.getObtenerDepartamentos import getObtenerDepartamentos
 from src.services.GET.getTest import getTests
-from src.services.GET.getTodosTest import getObtenerTodosTest
+from src.services.GET.getObtenerTestsEvaluables import getObtenerTestsEvaluables
 
 main = Blueprint('index_blueprint', __name__)
 
@@ -113,3 +116,91 @@ def registerTest():
   except Exception as e:
     return jsonify({'message':'ERROR', 'success':False, 'error': str(e)})
 
+@main.route("/obtenerPersona", methods = ['POST'])
+def obtenerPersona():
+  try:
+    data = request.get_json()
+    id_pers = data['id_pers']
+    
+    result = postObtenerPersona(id_pers)
+    if(result!=''):
+      data = {'persona': result.to_json()}
+      return jsonify({'message':'COMPLETE', 'success':True, 'data':data})
+    else:
+      return jsonify({'message':'ERROR', 'success':False})
+  except Exception as e:
+    return jsonify({'message':'ERROR', 'success':False, 'error': str(e)})
+
+@main.route("/obtenerDepartamentos")
+def obtenerDepartamento():
+  try:
+    result = getObtenerDepartamentos()
+    if(result!=''):
+      data = {'departamentos': result}
+      return jsonify({'message':'COMPLETE', 'success':True, 'data':data})
+    else:
+      return jsonify({'message':'ERROR', 'success':False})
+  except Exception as e:
+    return jsonify({'message':'ERROR', 'success':False, 'error': str(e)})
+
+@main.route("/obtenerProvincias", methods = ['POST'])
+def obtenerProvincia():
+  try:
+    data = request.get_json()
+    id_dep = data['id_dep']
+    
+    result = postObtenerProvincias(id_dep)
+    if(result!=''):
+      data = {'provincias': result}
+      return jsonify({'message':'COMPLETE', 'success':True, 'data':data})
+    else:
+      return jsonify({'message':'ERROR', 'success':False})
+  except Exception as e:
+    return jsonify({'message':'ERROR', 'success':False, 'error': str(e)})
+
+@main.route("/obtenerUbigeos", methods = ['POST'])
+def obtenerUbigeos():
+  try:
+    data = request.get_json()
+    id_prov = data['id_prov']
+    
+    result = postObtenerUbigeos(id_prov)
+    if(result!=''):
+      data = {'ubigeos': result}
+      return jsonify({'message':'COMPLETE', 'success':True, 'data':data})
+    else:
+      return jsonify({'message':'ERROR', 'success':False})
+  except Exception as e:
+    return jsonify({'message':'ERROR', 'success':False, 'error': str(e)})
+
+@main.route("/obtenerTestsEvaluables")
+def obtenerTestsEvaluables():
+  try:
+    result = getObtenerTestsEvaluables()
+    if(result!=''):
+      data = {'tests': result}
+      return jsonify({'message':'COMPLETE', 'success':True, 'data':data})
+    else:
+      return jsonify({'message':'ERROR', 'success':False})
+  except Exception as e:
+    return jsonify({'message':'ERROR', 'success':False, 'error': str(e)})
+
+@main.route("/registerDiagEsp", methods = ['POST'])
+def registerDiagEsp():
+  try:
+    data = request.get_json()
+    id_esp = data['id_usu']
+    id_test_res = data['id_test_res']
+    descripcion = data['descripcion']
+    resultado = data['resultado']
+    recomendacion = data['recomendacion']
+    tratamiento = data['tratamiento']
+    notas = data['notas']
+    
+    result = postRegisterDiagEsp(id_esp, id_test_res, descripcion, resultado, recomendacion, tratamiento, notas)
+    if(result):
+      return jsonify({'message':'COMPLETE', 'success':True})
+    else:
+      return jsonify({'message':'ERROR', 'success':False})
+  except Exception as e:
+    return jsonify({'message':'ERROR', 'success':False, 'error': str(e)})
